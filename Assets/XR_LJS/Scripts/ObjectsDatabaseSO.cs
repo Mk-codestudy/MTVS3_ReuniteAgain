146c2ccfcec5d93cb2242c31ced8c3afda4d8cd6
ORR-studio <jon589@naver.com>
2024-10-09 17:00:50 +0900

    프리팹이 소환되지 않는 오류 발견

--- Assets/XR_LJS/Scripts/ObjectsDatabaseSO.cs
+++ Assets/XR_LJS/Scripts/ObjectsDatabaseSO.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class ObjectsDatabaseSO : ScriptableObject
 {
     public List<ObjectData> objectData;
+    internal object objectsData;
 }
 
 [Serializable]
--- Assets/XR_LJS/Scripts/PlacementSystem.cs
+++ Assets/XR_LJS/Scripts/PlacementSystem.cs
@@ -19,9 +19,18 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     GameObject gridVisualization;
 
+    private GridData floorData, furnitureData;
+
+    private Renderer previewRenderer;
+
+    private List<GameObject> placedGameObject = new();
+
     private void Start()
     {
         StopPlacemet();
+        floorData = new();
+        furnitureData = new();
+        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
     }
 
     public void startPlacement(int ID) //https://youtu.be/i9W1kqUinIs?si=3vk3xC-v2FYZCUAW 
@@ -50,10 +59,29 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePosition = inputMGR.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
 
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedobjectIndex);
+        if (placementValidity == false)
+            return;
         // 선택 해제된 객체 인덱스를 선택하고 도트 프리팹을 추가할 것이므로 섹터 인덱스를 교체하여 게임 객체 프리팹 서버를 가져오는 방법
-        GameObject newObject = Instantiate(database.objectData[selectedobjectIndex].Prefab);
-        
+        GameObject newObject = Instantiate(database.objectData[selectedobjectIndex].Prefab);        
         newObject.transform.position = grid.CellToWorld(gridPosition);
+        placedGameObject.Add(newObject);
+        GridData selectedData = database.objectData[selectedobjectIndex].ID == 0 ?
+            floorData :
+            furnitureData;
+        selectedData.AddObjectAt(gridPosition,
+            database.objectData[selectedobjectIndex].Size,
+            database.objectData[selectedobjectIndex].ID,
+            placedGameObject.Count - 1);
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedobjectIndex)
+    {
+        GridData selectedData = database.objectData[selectedobjectIndex].ID == 0 ? 
+            floorData : 
+            furnitureData;
+
+        return selectedData.CanPlaceObjectAt(gridPosition, database.objectData[selectedobjectIndex].Size);
     }
 
     private void StopPlacemet()
@@ -71,6 +99,10 @@ public class PlacementSystem : MonoBehaviour
             return; 
         Vector3 mousePosition = inputMGR.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedobjectIndex);
+        previewRenderer.material.color = placementValidity ? Color.yellow : Color.red;
+
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
 
