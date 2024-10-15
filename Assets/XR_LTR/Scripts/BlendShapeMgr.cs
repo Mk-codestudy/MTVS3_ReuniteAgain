9a2b38ff1f2e7fa13dc0e3c0ffcc91e64867ea7a
ltae36 <ltae36@gmail.com>
2024-10-15 11:05:04 +0900

    커스터마이징

---
+++ Assets/XR_LTR/Scripts/BlendShapeMgr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlendShapeMgr : MonoBehaviour
+{
+    // 메쉬의 BlendShape옵션을 슬라이더에 연결하여 슬라이더를 통해 BlendShape 키 갑을 조정한다.
+
+    // 슬라이더 가져오기
+    public Slider slider1; //몸통
+    public Slider slider2; //머리
+
+    public FlexibleColorPicker fcp;
+    
+    // 커스텀할 오브젝트와 해당 오브젝트의 SkinnedMeshRenderer컴포넌트 불러오기
+    [SerializeField]
+    GameObject customObj;
+    Mesh mesh;
+    SkinnedMeshRenderer smr;
+    Material material;
+
+    void Start()
+    {
+        customObj = GameObject.FindWithTag("Player"); // 나중에는 서버에서 명령을 받아서 해당 prefab을 불러오는 것으로 변경
+        mesh = customObj.GetComponent<Mesh>();
+        smr = customObj.GetComponent<SkinnedMeshRenderer>();
+        material = smr.materials[1];
+    }
+
+    void Update()
+    {
+        //SkinnedMeshRenderer.SetBlendShapeWeight(조절할 BlendShape의 인덱스 번호, 해당하는 슬라이더의 value);
+
+        // 만약 0을 기준으로 
+        //value값이 음수에 가까워진다면 'skinny'수치가 증가, 
+        if (slider1.value < 0)
+        {
+            smr.SetBlendShapeWeight(4, slider1.value * -1); //몸매 날씬함
+            smr.SetBlendShapeWeight(3, slider2.value * -1); //얼굴 홀쭉함
+        }
+        //양수에 가까워진다면 'fat'값이 증가
+        else
+        {
+            smr.SetBlendShapeWeight(5, slider1.value); // 몸매 뚱뚱함
+            smr.SetBlendShapeWeight(2, slider2.value); // 얼굴 넙적함
+        }
+        material.color = fcp.color;
+    }
+}
---
+++ Assets/XR_LTR/Scripts/CameraSetting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetting : MonoBehaviour
+{
+    // 버튼을 눌러서 각도를 돌리기 vs 마우스를 드래그해서 회전하기
+    // 우클릭하여 드래그해서 오브젝트를 중심으로 카메라를 회전한다
+    public float mouseSecstv = 400f;
+    public float maxYrot = 70f;
+    public float minYrot = -70f;
+
+    public float moveSpeed = 0.5f;
+    public float rotationSpeed = -0.5f;
+
+    float mouseY = 90;
+    float mouseX = 30;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1)) MouseRotate();
+    }
+
+    void MouseRotate() 
+    {
+        mouseY += Input.GetAxisRaw("Mouse X") * mouseSecstv * Time.deltaTime;
+        mouseX -= Input.GetAxisRaw("Mouse Y") * mouseSecstv * Time.deltaTime;
+
+        // 상하 회전 각도를 제한하여 너무 뒤로 회전하지 않도록 합니다.
+        mouseX = Mathf.Clamp(mouseX, minYrot, maxYrot);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = mouseY;
+        rotation.x = mouseX;
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
