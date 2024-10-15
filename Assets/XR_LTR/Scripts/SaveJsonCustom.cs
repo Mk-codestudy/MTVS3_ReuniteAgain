c7ec12e63ff75129b5d27f525e7f9118f5bb3cd8
ltae36 <ltae36@gmail.com>
2024-10-15 18:31:32 +0900

    커스텀 정보 저장

---
+++ Assets/XR_LTR/Scripts/SaveJsonCustom.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SaveJsonCustom : MonoBehaviour
+{
+    public string saveFilePath;
+    public BlendShapeMgr bsm;
+
+    void Start()
+    {
+        saveFilePath = Application.persistentDataPath + "/customData.json";
+    }
+
+    public void saveButton() 
+    {
+        // 완료하기 버튼을 클릭하면 커스텀 패러미터 수치가 json으로 저장된다.
+    }
+    
+    void SaveCustomData(int index, float parameter, Color color) 
+    {
+        CharacterData character = LoadCharacterData();
+
+        CustomData newCustomData = new CustomData
+        {
+            idx = index,
+            pmtr = parameter,
+            col = color,
+        };
+        character.data.Add(newCustomData);
+
+        // Json변환 및 저장
+        string json = JsonUtility.ToJson(character);
+        System.IO.File.WriteAllText(saveFilePath, json);
+    }
+
+    private CharacterData LoadCharacterData() 
+    {
+        return new CharacterData(); // 데이터를 새로 생성
+    }
+}
+
+[System.Serializable]
+public class CustomData
+{
+    public int idx; // 블렌드셰이프
+    public float pmtr; // 슬라이더 수치값
+    public Color col; // 컬러값
+}
+
+[System.Serializable]
+public class CharacterData 
+{
+    public List<CustomData> data = new List<CustomData>(); // 커스텀 정보들 저장
+}
