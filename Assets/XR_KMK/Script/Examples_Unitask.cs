0b506932a26ec8bd88b18f21fe742708b91c329d
Mk-codestudy <sapphirestar.901@gmail.com>
2024-10-17 20:36:17 +0900

    유니태스크 매뉴얼 제작(2)

--- Assets/XR_KMK/Script/Examples_Unitask.cs
+++ Assets/XR_KMK/Script/Examples_Unitask.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Examples_Unitask : MonoBehaviour
 {
@@ -47,6 +49,37 @@ public class Examples_Unitask : MonoBehaviour
         Debug.Log("조건이 만족되기 전에는 여기까지만 실행되고요...");
         await UniTask.WaitUntil(()=> a == 5); //조건이 만족된다면 (int a == 5 라면)
         Debug.Log("만족되면 여기도 이어서 실행됩니다.");
+    }
+
+
+    //웹에 있는 이미지 가져오기
+    string imagepath; //URL 링크
+    RawImage img; //유니티로 보여줄 이미지
+
+    async UniTask<Texture2D> WaitGetTexture()
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(uri: imagepath);
+        await request.SendWebRequest();
+
+        if (request.result is UnityWebRequest.Result.ConnectionError or UnityWebRequest.Result.ProtocolError)
+        {
+            //실패일 때
+            Debug.LogError(request.error);
+        }
+        else
+        {
+            //성공했을 때
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            return texture;
+        }
+
+        return null; //이거 적어줘야 에러 사라져용
+    }
+
+    async UniTaskVoid Getimage()
+    {
+        Texture2D texture = await WaitGetTexture();
+        img.texture = texture;
     }
 
 }
