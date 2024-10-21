c814cac93338d27c78f3be7473569ea973802bc8
ORR-studio <jon589@naver.com>
2024-10-21 12:45:05 +0900

    그리드 확대중

---
+++ Assets/XR_LJS/Scripts/PosCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosCam : MonoBehaviour
+{
+   public Transform cameraTransform;
+    public float moveSpeed = 5f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        if (cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+        offset = cameraTransform.position - transform.position;
+    }
+
+    void Update()
+    {
+
+        // 카메라 이동
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        // 카메라 위치 업데이트
+        cameraTransform.position = transform.position + offset;
+        cameraTransform.rotation = Quaternion.Euler(30f, 0f, 0f); // 원하는 고정 각도로 설정
+        // 카메라가 항상 CameraFocus를 바라보도록 설정
+        cameraTransform.LookAt(transform);
+    }
+}
+
