a80fe3b2af6029918d44794bf5eb07d60c57fa2b
ORR-studio <jon589@naver.com>
2024-10-22 18:31:41 +0900

    FSM 시작전 백업

---
+++ Assets/XR_LJS/Scripts/NPCFSM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFSM : MonoBehaviour
+{
+    public enum NPCState
+    {
+        idle =0,
+        walk =1,
+        jump =2,
+        run =4,
+        trace=8, 
+    }
+}
